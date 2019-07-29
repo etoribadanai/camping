0d8aa2ca01eed52c37c65bdca341ede0e0f2c99b
Etori <etori@Mac-mini-de-suporte.local>
2019-07-29 16:06:54 -0300

    fix table

--- priv/repo/migrations/20190729190149_add_name_category_to_products.exs
+++ priv/repo/migrations/20190729190149_add_name_category_to_products.exs
@@ -2,8 +2,10 @@ defmodule Camping.Repo.Migrations.AddNameCategoryToProducts do
   use Ecto.Migration
 
   def change do
+    rename table(:products), :description, to: :name
+
     alter table(:products) do
-      add(:name, :string)
+      add(:description, :string)
       add(:category, :string)
     end
   end
