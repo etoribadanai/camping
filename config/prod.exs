bb51f297681bcd2eb56546b0c1e690f7899e066e
etoribadanai <etoribadanai@gmail.com>
2019-09-20 18:32:14 -0300

    config prod

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :camping, CampingWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "camping-env.tamfv6m3tv.sa-east-1.elasticbeanstalk.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 config :logger, level: :info
