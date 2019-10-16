cea1ed4a81b43ae507e1aa4e0ab531d1b5062e5e
etoribadanai <etoribadanai@gmail.com>
2019-10-15 23:21:21 -0300

    add option to questions endpoint

--- lib/camping_web/views/question_view.ex
+++ lib/camping_web/views/question_view.ex
@@ -1,3 +1,5 @@
+require IEx
+
 defmodule CampingWeb.QuestionView do
   use CampingWeb, :view
   alias CampingWeb.QuestionView
@@ -12,9 +14,21 @@ defmodule CampingWeb.QuestionView do
 
   def render("question.json", %{question: question}) do
     %{
+      id: question.id,
       question_desc: question.question_desc,
       position: question.position,
-      required: question.required
+      required: question.required,
+      options: render_many(question.options, QuestionView, "option.json")
+    }
+  end
+
+  def render("option.json", %{question: option}) do
+    %{
+      question_id: option.question_id,
+      option: option.option,
+      id: option.id,
+      position: option.position,
+      active: option.active
     }
   end
 end
